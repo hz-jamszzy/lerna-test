e2934354bcf2024b65ecf4a49413bf9a44de85d4
onePunch <zhanziyan@jravity.com>
2022-01-21 11:01:32 +0800

    [+] 增加james-test-a的ts声明

---
+++ packages/james-test-a/main/index.d.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: onePunch
+ * @Date: 2022-01-21 10:56:06
+ * @LastEditors: onePunch
+ * @LastEditTime: 2022-01-21 11:00:04
+ * @Description: file content
+ * @FilePath: \lerna-test\packages\james-test-a\main\index.d.ts
+ */
+declare module 'james-test-a' {
+  export const parseIntSum: (a: number, b: number) => string;
+}
