384c5c441c9fa47775fc581a74791c5a25b87272
hz-jameszzy <zziyan123@163.com>
2022-04-06 14:18:08 +0800

    [+] james-test-b新增功能函数joinFn

--- packages/james-test-b/main/index.js
+++ packages/james-test-b/main/index.js
@@ -2,17 +2,32 @@
  * @Author: onePunch
  * @Date: 2022-01-20 15:37:26
  * @LastEditors: onePunch
- * @LastEditTime: 2022-01-20 15:37:26
+ * @LastEditTime: 2022-04-06 10:22:43
  * @Description: file content
- * @FilePath: \lerna-test\packages\test-b\main\index.js
+ * @FilePath: \lerna-test\packages\james-test-b\main\index.js
  */
-import { add } from 'lodash';
+import { add, join } from 'lodash';
 
-const checkType = (type, value) => {
-  return typeof value === type;
+const types = ['Arguments', 'Function', 'Array', 'Object', 'String', 'Number', 'Date', 'RegExp', 'Error', 'Symbol', 'Map', 'WeakMap', 'Set', 'WeakSet']
+const checkTypeFns = {}
+types.forEach(v => {
+  checkTypeFns[`is${v}`] = obj => {
+    return Reflect.apply(Object.prototype.toString, obj, []) === `[object ${v}]`
+  }
+})
+
+export const joinFn = (arr, connector) => {
+  if(!checkTypeFns['isArray'](arr) || checkTypeFns['isString'](connector)) {
+    console.log('入参类型不对');
+    return;
+  };
+  return join(arr, connector);
 }
 
 export const sumFn = (a, b) => {
-  if(!checkType('number', a) || !checkType('number', b)) return;
+  if(!checkTypeFns['isNumber'](a) || !checkTypeFns['isNumber'](b)) {
+    console.log('入参类型不对');
+    return;
+  };
   return add(a, b);
 }
